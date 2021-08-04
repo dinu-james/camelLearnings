964e395ab240c01f3f9f969c1c797e7e512fd354
Dinu Kolath James <Dinu.Kolath.James@gds.ey.com>
2021-08-04 10:15:30 +0530

    Initial commit

---
+++ camel-microservice-a/camel-microservice-a/src/main/java/com/dkj/microservice/camelmicroservicea/routes/a/TimerRouter.java
@@ -0,0 +1,10 @@
+package com.dkj.microservice.camelmicroservicea.routes.a;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class TimerRouter extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
---
+++ camel-microservice-a/camel-microservice-a/src/test/java/com/dkj/microservice/camelmicroservicea/CamelMicroserviceAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkj.microservice.camelmicroservicea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
