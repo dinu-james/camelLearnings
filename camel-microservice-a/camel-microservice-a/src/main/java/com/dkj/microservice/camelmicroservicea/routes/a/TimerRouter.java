ce87ae274005d7dcae17f0df070e4c102d5ea2aa
Dinu Kolath James <Dinu.Kolath.James@gds.ey.com>
2021-08-04 10:34:02 +0530

    Route added.

--- camel-microservice-a/camel-microservice-a/src/main/java/com/dkj/microservice/camelmicroservicea/routes/a/TimerRouter.java
+++ camel-microservice-a/camel-microservice-a/src/main/java/com/dkj/microservice/camelmicroservicea/routes/a/TimerRouter.java
@@ -1,10 +1,14 @@
 package com.dkj.microservice.camelmicroservicea.routes.a;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TimerRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-
+        from("timer:first-timer").
+                transform().constant("Hello").
+                to("log:first-timer");
     }
 }
